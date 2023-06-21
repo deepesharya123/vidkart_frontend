53ac848a700097414e26ea9ccdc04db1a2d0c8a1
deepesharya123 <deepesharya82246@gmail.com>
2023-06-21 19:27:32 +0530

    uploading and logout are working with react

--- src/components/LandingSeller.js
+++ src/components/LandingSeller.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 
@@ -10,15 +10,23 @@ import Order from "../images/order.png";
 import Sold from "../images/sold.png";
 import Customer from "../images/customer.png";
 import PreviousItem from "../images/previous_item.png";
+import { useCookies } from "react-cookie";
 const backend = "http://localhost:8080";
 
 const Header = (props) => {
   const { user } = props;
   const [search, SetSearch] = useState("");
+  const [cookies, setCookie, removeCookie] = useCookies(["access_token"]);
   const location = useLocation();
-  const userDetails = location.state;
+  const userData = location.state;
+  const [userDetails, setUserDetails] = useState(userData);
   const navigate = useNavigate();
-  console.log("userdetails sending to lotoug", userDetails);
+
+  useEffect(() => {
+    setUserDetails(!userDetails ? userData : userDetails);
+  }, []);
+
+  console.log("userDetails ,", userDetails);
   const handleSearch = (e) => {
     const { value } = e.target;
     SetSearch(value);
@@ -46,17 +54,17 @@ const Header = (props) => {
           userDetails,
           {
             withCredentials: true,
-            // credentials: "include",
-            // headers: { "Content-Type": "multipart/form-data" },
           }
         )
         .then((res) => {
           if (res.status != 200)
             throw new Error("There is some error during logging out");
+          removeCookie("auth_token");
           navigate("/");
         })
         .catch((err) => {
           // alert("Error", err);
+          alert("Some error occured during logout");
           console.log("handling the error logout of seller ", err);
         });
     };
@@ -102,6 +110,28 @@ const Header = (props) => {
 
 const Squareinfo = (props) => {
   const { image, count, description, modifications, color: color } = props.item;
+  const allPreviousItems = props.allPreviousItems;
+
+  const [cookies, setCookie, removeCookie] = useCookies(["access_token"]);
+
+  const handlePreviousItems = () => {
+    console.log("See Previous Items");
+    const getPreviousItems = async () => {
+      // const token = cookies.auth_token;
+      // console.log("token is ", token);
+      await axios
+        .post(`${backend}/users/previousItem`, { token: cookies.auth_token })
+        .then((res) => {
+          allPreviousItems(res.data);
+          console.log("All previous items are", res);
+        })
+        .catch((err) => {
+          console.log("Some error occured during fetching previous items", err);
+        });
+    };
+    getPreviousItems();
+  };
+
   return (
     <div className="squares">
       <img src={image} className="image_seller_dashboard" />
@@ -111,7 +141,12 @@ const Squareinfo = (props) => {
           <div className="show_button">
             <div>{description}</div>
             {description === "Previous Items" && (
-              <button className="previous_items_seller ">Clcik me</button>
+              <button
+                className="previous_items_seller "
+                onClick={handlePreviousItems}
+              >
+                Click me
+              </button>
             )}
           </div>
         }
@@ -122,6 +157,8 @@ const Squareinfo = (props) => {
 };
 
 const UploadItem = (props) => {
+  const [cookies, setCookie, removeCookie] = useCookies(["access_token"]);
+
   const [itemData, setItemData] = useState({
     title: "",
     college: "",
@@ -130,19 +167,70 @@ const UploadItem = (props) => {
     ProductUpload: "",
   });
 
+  const [image, setImage] = useState("");
+
   const handleChange = (e) => {
     e.preventDefault();
     const { name, value } = e.target;
-    console.log({ name, value, len: value.length });
     setItemData({ ...itemData, [name]: value });
+    if (e.target.type == "file") {
+      const reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+      reader.onload = () => {
+        console.log(reader.result);
+        setImage({ [name]: reader.result });
+        setItemData({ ...itemData, ProductUpload: reader.result });
+      };
+      reader.onerror = (err) => {
+        console.log("err", err);
+      };
+    }
   };
 
-  console.log(itemData);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log("itemData from upload button click", itemData);
+
+    const uploadItem = async () => {
+      itemData.token = cookies.auth_token;
+      console.log("while uploading the item , item is here", {
+        itemData,
+      });
+
+      await axios
+        .post(`${backend}/users/uploadItem`, itemData, {})
+        .then((res) => {
+          if (res.status !== 200) throw new Error("Please try again!");
+          alert(res.data.message);
+          console.log("Message:", res.data.message);
+        })
+        .catch((err) => {
+          alert(
+            "Something went wrong while product uploading!,Please try again!"
+          );
+          console.log("error while uploading item from frontend", err);
+        });
+    };
+
+    uploadItem();
+
+    setItemData({
+      title: "",
+      college: "",
+      description: "",
+      price: "",
+      ProductUpload: "",
+    });
+  };
 
   return (
     <div className="upload_section">
       <div className="form_upload">
-        <form onChange={handleChange}>
+        <form
+          onChange={handleChange}
+          onSubmit={handleSubmit}
+          enctype="multipart/form-data"
+        >
           <input
             type="text"
             required
@@ -186,12 +274,37 @@ const UploadItem = (props) => {
           </button>
         </form>
       </div>
+      {/* show uploading item data */}
+      {/* {itemData.ProductUpload != "" && itemData.ProductUpload && (
+        <img src={itemData.ProductUpload} />
+      )} */}
+    </div>
+  );
+};
+
+const ShowItems = (props) => {
+  const { college, description, imagePath, owner, phoneNumber, price, title } =
+    props.item;
+  console.log({ item: props.item });
+  return (
+    <div className="previous_item">
+      <img src={imagePath} className="item_image" />
+      <div className="item_info">
+        {" "}
+        <div className="title_item">{title}</div>
+        <div>Buy @ Only{price}</div>
+      </div>
     </div>
   );
 };
 
 function LandingSeller(props) {
   const { user } = props;
+
+  const [previousItems, setPreviousItems] = useState([]);
+  const allPreviousItems = (allitems) => {
+    setPreviousItems(allitems);
+  };
   const data = [
     {
       image: Sales,
@@ -229,6 +342,9 @@ function LandingSeller(props) {
       color: "#86959E",
     },
   ];
+
+  console.log("previousItems from main header", previousItems);
+
   return (
     <div>
       <Header user={user} />
@@ -236,11 +352,19 @@ function LandingSeller(props) {
         <h1 className="today_data">Today's Data</h1>
         <div className="square_padding">
           {data.map((item) => {
-            return <Squareinfo item={item} />;
+            return (
+              <Squareinfo item={item} allPreviousItems={allPreviousItems} />
+            );
           })}
         </div>
       </div>
       <UploadItem />
+      <div className="previous_items_list">
+        {previousItems?.length > 0 &&
+          previousItems.map((item) => {
+            return <ShowItems item={item} />;
+          })}
+      </div>
     </div>
   );
 }
