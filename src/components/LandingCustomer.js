72cf2f5438a4f18961ab37a7331479986c0831c6
deepesharya123 <deepesharya82246@gmail.com>
2023-07-04 22:04:26 +0530

    logout error resolved

--- src/components/LandingCustomer.js
+++ src/components/LandingCustomer.js
@@ -53,10 +53,7 @@ const Header = (props) => {
       await axios
         .post(
           `${backend}/${user === "seller" ? "users" : "customer"}/logout`,
-          userDetails,
-          {
-            withCredentials: true,
-          }
+          userDetails
         )
         .then((res) => {
           if (res.status != 200)
